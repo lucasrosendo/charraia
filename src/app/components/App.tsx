ac74e3fc9eacdf8d96accdfbf9743e4360a9c431
Lucas Rosendo <lucasrosendo91@gmail.com>
2024-03-15 00:37:12 -0300

    feat: ta andando

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -14,6 +14,11 @@ const ListaPresentes = () => {
   const [cama, setCama] = useState<Item[]>([]);
   const [util, setUtil] = useState<Item[]>([]);
   const [vidros, setVidros] = useState<Item[]>([]);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleDropdownClick = () => {
+    setShowDropdown(!showDropdown);
+  };
   const fetchPanelas = async () => {
     try {
       const response = await axios.get('./data/panelas.json');
@@ -73,45 +78,30 @@ const ListaPresentes = () => {
   }, []);
 
   return (
-    <div className='flex flex-col bg-gray-600 items-center'>
+    <div className='flex flex-col bg-gradient-to-r from-cyan-500 to-blue-500 items-center'>
       <h1 className=''>Lista de Presentes</h1>
-      <ul>
-        <h3>VIDROS E LOUÇAS</h3>
-        {vidros.map((item) => (
-          <li className=''
-            key={item.id}>{item.item}</li>
-        ))}
-      </ul>
-      <ul>
-        <h3>INOX SIMILARES AÇO INOX / LOUÇA / CERÂMICA</h3>
-        {inox.map((item) => (
-          <li key={item.id}>{item.item}</li>
-        ))}
-      </ul>
-      <ul>
-        <h3>UTILIDADES DOMÉSTICAS DIVERSAS</h3>
-        {util.map((item) => (
-          <li key={item.id}>{item.item}</li>
-        ))}
-      </ul>
-      <ul>
-        <h3>PANELAS</h3>
-        {panelas.map((item) => (
-          <li key={item.id}>{item.item}</li>
-        ))}
-      </ul>
-      <ul>
-        <h3>ELETRODOMÉSTICOS</h3>
-        {eletro.map((item) => (
-          <li key={item.id}>{item.item}</li>
-        ))}
-      </ul>
-      <ul>
-        <h3>CAMA MESA E BANHO</h3>
-        {cama.map((item) => (
-          <li key={item.id}>{item.item}</li>
-        ))}
-      </ul>
+
+      <div>
+        <h3 className='cursor-pointer' onClick={handleDropdownClick}>VIDROS E LOUÇAS</h3>
+        {showDropdown && (
+          vidros.map((item) => (
+            <div
+              className='gap-2 flex justify-between items-center w-full p-2 bg-white rounded-md m-2'
+              key={item.id}>{item.item}
+              <div className='flex gap-4 items-center'>
+                <input className='rounded-md h-8 p-2 w-60 bg-slate-200' type="text" placeholder='Coloque seu nome aqui' />
+                <button className='bg-green-300 rounded-md h-8 w-14'>salvar</button>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+      <h3>INOX SIMILARES AÇO INOX / LOUÇA / CERÂMICA</h3>
+      <h3>UTILIDADES DOMÉSTICAS DIVERSAS</h3>
+      <h3>PANELAS</h3>
+      <h3>ELETRODOMÉSTICOS</h3>
+      <h3>CAMA MESA E BANHO</h3>
+
     </div>
   );
 };
